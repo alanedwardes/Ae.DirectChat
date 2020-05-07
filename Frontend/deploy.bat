e8e51a132a8d0c57024149b345d7b72b9f3844b9
Alan Edwardes <alan@alanedwardes.com>
2020-05-07 21:21:43 +0100

    Tweaks deploy script to explicitly specify the mime types.

--- Frontend/deploy.bat
+++ Frontend/deploy.bat
@@ -1,3 +1,6 @@
 call build.bat
-aws s3 cp dist s3://ae-chat --acl public-read --recursive --cache-control no-cache
+aws s3 cp dist/bundle.js  s3://ae-chat --acl public-read --cache-control no-cache --content-type application/javascript
+aws s3 cp dist/index.html s3://ae-chat --acl public-read --cache-control no-cache --content-type text/html
+aws s3 cp dist/robots.txt s3://ae-chat --acl public-read --cache-control no-cache --content-type text/plain
+aws s3 cp dist/style.css  s3://ae-chat --acl public-read --cache-control no-cache --content-type text/css
 pause
